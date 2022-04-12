376d1bda379ff79da1436f2ef6f294e01bfd5416
vohidjon94 <vitcoderuz@gmail.com>
2022-04-12 17:26:02 +0500

    1725

--- backend/views.py
+++ backend/views.py
@@ -1,8 +1,15 @@
+from multiprocessing import context
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 def home(request):
-    return render(request, 'index.html')
+    posts =Post.objects.all()
+    context = {
+        "posts":posts
+    }
+    return render(request, 'index.html', context)
 
 def about(request):
     return render(request, 'about.html')
+
