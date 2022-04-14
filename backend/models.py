a14280bb035b60e154b7cab35da690c93f368749
vohidjon94 <vitcoderuz@gmail.com>
2022-04-14 08:19:32 +0500

    update8:19

--- backend/models.py
+++ backend/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class Post(models.Model):
     nomi = models.CharField(max_length=20)
+    rasm = models.ImageField()
 
     def __str__(self):
         return self.nomi
