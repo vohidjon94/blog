dfd56761d7598b65336cceec3b0f4d63cc90b03c
vohidjon94 <vitcoderuz@gmail.com>
2022-04-14 08:37:01 +0500

    update8:36

--- backend/models.py
+++ backend/models.py
@@ -1,10 +1,11 @@
+from distutils.command.upload import upload
 from django.db import models
 
 # Create your models here.
 
 class Post(models.Model):
     nomi = models.CharField(max_length=20)
-    rasm = models.ImageField()
+    rasm = models.ImageField(upload_to = "rasmlar/")
 
     def __str__(self):
         return self.nomi
