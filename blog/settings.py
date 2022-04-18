52f8af32417c92766fc77f1a729380e86a2e6de6
vohidjon94 <vitcoderuz@gmail.com>
2022-04-18 18:29:08 +0500

    aws3

--- blog/settings.py
+++ blog/settings.py
@@ -115,10 +115,18 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static', 
 ]
 
-MEDIA_ROOT = BASE_DIR/"media"
-MEDIA_URL = "/media/"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+
+AWS_ACCESS_KEY_ID = 'AKIA542DU3LWSUI6FEWH'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'blogpictures94'
+AWS_QUERYSTRING_AUTH = False
